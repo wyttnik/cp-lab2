6da62c686d699a0fbda0a6de3ede50532e1e82c8
wyttnik <81494616+wyttnik@users.noreply.github.com>
2022-09-28 01:21:10 +0300

    Update cache.js

--- src/cache.js
+++ src/cache.js
@@ -4,6 +4,7 @@ class Cache{
     }
 
     set = (key, value, count = 1) => {
+        if (key === undefined) return;
         if (count <= 0) return;
         else this._cache.set(key, {val: value, refCount: count});
     };
@@ -12,14 +13,12 @@ class Cache{
         const res = this._cache.get(key);
         // if key is missing -> null
         if (res === undefined) return null;
-        // if val is missing return null but keep such key and lower ref
         this._cache.set(key, {val: res.val, refCount: res.refCount - 1});
         if (res.refCount - 1 === 0) this._cache.delete(key);
         return res.val === undefined ? null : res.val;
     };
 
     getStatistic() {
-        console.log(this._cache);
         const arr = [];
         this._cache.forEach((value,key) => arr.push({key,
             value:value.val,
